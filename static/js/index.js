24c07bae2f02fcfe90203c9d737f690aa98d6645
Hector-654 <69879591+Hector-654@users.noreply.github.com>
2020-09-02 15:37:22 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,22 @@
 //https://www.eclipse.org/paho/clients/js/
 
 function LED1_On() {
-	alert("led on");
+	//alert("led on");
 	console.log("led on");
-	document.getElementById("sensor").innerHTML="led on";
+	//document.getElementById("sensor").innerHTML="led on";
+	message =new Paho.MQTT.Message("LED1_ALTO");
+	message.destinationName = "654hector1@gmail.com/kop";
+	client.send(message);
   
 }
 function LED1_Off(){	
-	alert("led off");
+	//alert("led off");
 	console.log("led off");
-	document.getElementById("sensor").innerHTML="led off";
+	//document.getElementById("sensor").innerHTML="led off";
+	message =new Paho.MQTT.Message("LED1_ALTO");
+	message.destinationName = "654hector1@gmail.com/kop";
+	client.send(message);
+	
 }
 
 
